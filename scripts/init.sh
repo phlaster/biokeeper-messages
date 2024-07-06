9dbe221713bbbbc19965a3f1b45fda167cebbf68
catfield123 <gtagta65@gmail.com>
2024-07-07 02:44:55 +0300

    basic initialization: - creates separate users for core and auth services - creates basic virtual host - creates exchange for users - creates queue for transfering new users to core_backend
    * initializations starts only when rabbitmq server is started

---
+++ scripts/init.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# wait for rabbitmq server to start
+/scripts/wait_for_rabbitmq.sh localhost 5672 60
+
+echo "STARTING INITIALIZATION"
+
+
+rabbitmqctl set_user_tags $RABBITMQ_DEFAULT_USER administrator
+
+#
+# DECLARE EXCHANGES, QUEUES AND BINDINGS
+#
+
+# creating basic_vhost vhost for microservice communication
+rabbitmqctl add_vhost basic_vhost
+rabbitmqctl set_permissions -p basic_vhost $RABBITMQ_DEFAULT_USER ".*" ".*" ".*"
+
+## exchange users
+rabbitmqadmin -u $RABBITMQ_DEFAULT_USER -p $RABBITMQ_DEFAULT_PASS -V basic_vhost declare exchange name=users.topic type=direct
+
+## core.new_user queue (for sending new users for core service)
+rabbitmqadmin -u $RABBITMQ_DEFAULT_USER -p $RABBITMQ_DEFAULT_PASS -V basic_vhost declare queue name=core.new_user durable=true
+
+## binding for sending new user to queue from exchange
+rabbitmqadmin -u $RABBITMQ_DEFAULT_USER -p $RABBITMQ_DEFAULT_PASS -V basic_vhost declare binding source=users.topic destination=core.new_user destination_type=queue routing_key=new_user
+
+
+#
+# CREATE USERS FOR MICROSERVICES
+#
+
+# creating of user for auth service
+rabbitmqctl add_user $AUTH_USER $AUTH_PASS 2>/dev/null
+
+# creating of user for core service
+rabbitmqctl add_user $CORE_USER $CORE_PASS 2>/dev/null
+
+
+#
+# SET PERMISSIONS FOR USERS
+#
+
+# allow AUTH_USER to write messages to users.topic with routing_key = new_user
+rabbitmqctl set_topic_permissions -p basic_vhost $AUTH_USER users.topic "^new_user" "^&"
+
+# allow CORE_USER to read messages from users.topic with routing_key = new_user
+rabbitmqctl set_topic_permissions -p basic_vhost $CORE_USER users.topic "^&" "^new_user"
+
+
+echo INITIALIZATION COMPLETED
